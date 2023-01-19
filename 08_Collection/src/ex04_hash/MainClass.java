5d6d544136f0f9a099cedd2f8184aa082a11f529
o-dim <omiia0407@gmail.com>
2023-01-19 17:19:19 +0900

    2023-01-19

---
+++ 08_Collection/src/ex04_hash/MainClass.java
@@ -0,0 +1,22 @@
+package ex04_hash;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		
+		User user1 = new User("admin", "12323");
+		User user2 = new User("admin", "12323");
+
+		Set<User> set = new HashSet<User>();
+		set.add(user1);
+		set.add(user2);
+		
+		System.out.println(set);
+		
+	}
+
+}
