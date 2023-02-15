051a013c619ffdcab92ad795adf60dd1c8f244b6
o-dim <omiia0407@gmail.com>
2023-02-15 17:23:44 +0900

    2023-02-15

--- Appendix03_Jdbc/src/ex01_connection/MainClass.java
+++ Appendix03_Jdbc/src/ex01_connection/MainClass.java
@@ -1,56 +1,74 @@
 package ex01_connection;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class MainClass {
 	
-	public static void ex01() {
+	public static void ex03() {
+		BufferedReader reader = null;
+		Connection con = null;
 		
 		try {
 			
-			Class.forName("oracle.jdbc.OracleDriver");
-			System.out.println("클래스가 로드되었습니다~~~!!!!~!");
+			// 프로퍼티 파일을 읽는 문자 입력 스트림 생성하기
+			reader = new BufferedReader(new FileReader("db.properties"));
 			
-		} catch (ClassNotFoundException e) {
+			// Properties 파일 읽어서 사용자 정보 처리하기 
+			Properties properties = new Properties();
+			properties.load(reader);
+			
+			// 프로퍼티 객체에 저장된 각 Property 읽기
+			String url = properties.getProperty("url");
+			String user = properties.getProperty("user");
+			String password = properties.getProperty("password");
+			
+			// DriverManager로 부터 Connection 객체 얻기
+			con = DriverManager.getConnection(url, user, password);
+			System.out.println("db접속완.");
+			
+		} catch (IOException e) {
 			e.printStackTrace();
-			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(con != null) {
+					con.close();
+				}
+			} catch (SQLException e2) {
+				e2.printStackTrace();
+				}
 		}
+		
 		
 	}
 	
-	public static void ex02() {
+	public static Connection getConnection() throws Exception {
 		
 		Connection con = null;
 		
-		// 오라클 접속 정보
-		String url = "jdbc:oracle:thin:@localhost:1521:xe";
-		String user = "GDJ61";
-		String password = "1111";
+		Class.forName("oracle.jdbc.OracleDriver");
 		
-		// DriverManager 클래스로부터 Connection 객체 받아오기
-		try {
-			con = DriverManager.getConnection(url, user, password);
-			System.out.println("DB에 접속완료^.^");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		Properties properties = new Properties();
+		properties.load(new BufferedReader(new FileReader("db.properties")));
 		
-		// 사용이 끝난 Connection 객체는 반드시 닫아야한다.
-		try {
-			if(con != null) {
-				con.close();
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		con = DriverManager.getConnection(properties.getProperty("url"), properties.getProperty("user"), properties.getProperty("password"));
+				
+		return con;
 		
 	}
 	
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws Exception {
 		
-		ex02();
+		Connection con = getConnection();
+		System.out.println("DB에 접속 완.");
 		
 	}
 
